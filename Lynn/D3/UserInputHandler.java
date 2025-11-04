e5a4c44e7fa4efed0bfbd8ea33269ebab4742e88
LynnRuchti <lynn.ruchti@edu.tbz.ch>
2025-11-04 17:26:37 +0100

    finishing D3

--- Lynn/D3/UserInputHandler.java
+++ Lynn/D3/UserInputHandler.java
@@ -1,7 +1,25 @@
 import java.util.Scanner;
 
 public class UserInputHandler {
-    public String getCityFromUser() throws InvalidCityException {
+    public  UserInputHandler()  {
+        WeatherService weatherService = new WeatherService();
+        WeatherLogic logic = new WeatherLogic(weatherService);
+
+        while (true) {
+            try {
+                String city = getCityFromUser();
+                if (city.equals("q")) {
+                    System.exit(0);
+                }
+                String report = logic.getWeatherReport(city);
+                System.out.println(report);
+            } catch (InvalidCityException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+    }
+
+    private String getCityFromUser() throws InvalidCityException{
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter a city (exit with q): ");
         String city = scanner.nextLine().trim();
@@ -9,7 +27,6 @@ public class UserInputHandler {
         if (city.length() < 2 && !city.toLowerCase().equals("q")) {
             throw new InvalidCityException("Invalid city input! Please enter at least 2 letters.");
         }
-
         return city;
     }
 }
